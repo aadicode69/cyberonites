d311ebde71b75d31513a959638515a6cc013c39f
Vaibhav-Singh2 <mathura.vs@gmail.com>
2024-10-13 21:00:22 +0530

    changing size of svgs to make more resposive

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 import HeroSection from "./components/hero-section/hero-section";
-import { Navbar } from "./components/navbar/navbar";
+// import { Navbar } from "./components/navbar/navbar";
 function App() {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
       <HeroSection />
       <Body />
       <Footer />
