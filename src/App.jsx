2de12240fb0a9901305975b8159a9e0f813f9e38
MAYANKSHARMA <sharmaji123mayank@gmail.com>
2024-10-14 12:09:37 +0530

    Alumini-Sponser

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 import { Navbar } from "./components/navbar/navbar";
+import Aluminis from "./components/Alumini/Alumni";
+import Sponsor from "./components/Sponsers/Sponser";
+
+
 function App() {
   return (
     <div>
       <Navbar />
+      <Aluminis heading="Our Alumni" />
+      <Sponsor heading="Our Sponsors" />
       <Body />
       <Footer />
     </div>
