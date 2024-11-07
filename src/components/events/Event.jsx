a9d983de295c25d98301d19a8e7aaf349c3e1c78
ManmohanSinghRaghav <manmohan0singh0@gmail.com>
2024-11-07 22:36:07 +0530

    Refactor Event component layout; enhance event display with improved Swiper integration and styling

--- src/components/events/Event.jsx
+++ src/components/events/Event.jsx
@@ -28,151 +28,167 @@ const Event = () => {
   }, []);
 
   return (
-    <div
-      className={`flex ${
-        direction ? "flex-col" : "flex-row"
-      } m-4 p-8 border border-gray-800 bg-gray-900 rounded`}
-    >
-      <Swiper
-        onSwiper={setSecondSwiper}
-        direction="horizontal"
-        loop={true}
-        effect={"cards"}
-        grabCursor={true}
-        autoplay={{
-          delay: 2000,
-          disableOnInteraction: false,
-        }}
-        keyboard={{
-          enabled: true,
-        }}
-        controller={{ control: firstSwiper }}
-        modules={[Keyboard, Autoplay, EffectCards, Controller]}
-        className="mySwiper w-72 h-96"
-      >
-        {Events.map((eve) => (
-          <SwiperSlide
-            key={eve.title}
-            className="flex items-center justify-center"
+    <>
+      <div className="flex flex-col items-center justify-center my-10">
+        <h1 className="text-cyan-400 font-cyberfont font-semibold text-4xl mt-10">
+          {/* Our Events */}
+          Event Archive
+        </h1>
+        <div
+          className={` w-2/3 flex ${
+            direction ? "flex-col" : "flex-row"
+          } m-4 p-8 border border-gray-800 bg-gray-900 rounded`}
+        >
+          <Swiper
+            onSwiper={setSecondSwiper}
+            direction="horizontal"
+            loop={true}
+            effect={"cards"}
+            grabCursor={true}
+            autoplay={{
+              delay: 2000,
+              disableOnInteraction: false,
+            }}
+            keyboard={{
+              enabled: true,
+            }}
+            controller={{ control: firstSwiper }}
+            modules={[Keyboard, Autoplay, EffectCards, Controller]}
+            className="mySwiper w-72 h-96"
           >
-            {eve.image && (
-              <img
-                className="object-contain cursor-pointer h-full w-full"
-                src={eve.image}
-                alt={eve.title}
-              />
-            )}
-          </SwiperSlide>
-        ))}
-        <div className={`absolute left-1/2 ${ direction ? "-translate-x-1/2 top-[-1.5rem]" : "-translate-x-1/2 bottom-[-5.5rem]"}  w-18 h-20 text-cyan-400 opacity-60 flex`}>
-          <div className="h-5 -rotate-90">
-            <FaAngleDoubleUp className="text-sm animate-[bounce_1s_.5s_infinite]" />
-          </div>
-          <div className="text-sm font-mono font-thin animate-pulse">Swipe</div>
-          <div className="h-5 rotate-90">
-            <FaAngleDoubleUp className="text-sm animate-[bounce_1s_.5s_infinite]" />
-          </div>
-        </div>
-      </Swiper>
+            {Events.map((eve) => (
+              <SwiperSlide
+                key={eve.title}
+                className="flex items-center justify-center"
+              >
+                {eve.image && (
+                  <img
+                    className="object-contain cursor-pointer h-full w-full"
+                    src={eve.image}
+                    alt={eve.title}
+                  />
+                )}
+              </SwiperSlide>
+            ))}
+            <div
+              className={`absolute left-1/2 ${
+                direction
+                  ? "-translate-x-1/2 top-[-1.5rem]"
+                  : "-translate-x-1/2 bottom-[-5.5rem]"
+              }  w-18 h-20 text-cyan-400 opacity-60 flex`}
+            >
+              <div className="h-5 -rotate-90">
+                <FaAngleDoubleUp className="text-sm animate-[bounce_1s_.5s_infinite]" />
+              </div>
+              <div className="text-sm font-mono font-thin animate-pulse">
+                Swipe
+              </div>
+              <div className="h-5 rotate-90">
+                <FaAngleDoubleUp className="text-sm animate-[bounce_1s_.5s_infinite]" />
+              </div>
+            </div>
+          </Swiper>
 
-      {direction ? (
-        <Swiper
-          onSwiper={setFirstSwiper}
-          effect={"creative"}
-          loop={true}
-          direction="vertical"
-          grabCursor={true}
-          creativeEffect={{
-            prev: {
-              shadow: true,
-              translate: [0, -400, 0],
-            },
-            next: {
-              translate: [0, 0, -400],
-            },
-          }}
-          controller={{ control: secondSwiper }}
-          modules={[Controller, EffectCreative]}
-          className="mySwiper w-72 h-96 text-white border-t-2 border-cyan-600"
-        >
-          {Events.map((eve) => (
-            <SwiperSlide
-              key={eve.title}
-              className="flex w-72 text-center p-4 flex-col items-center justify-between bg-slate-900 border-b border-x border-gray-600 rounded-b-lg"
+          {direction ? (
+            <Swiper
+              onSwiper={setFirstSwiper}
+              effect={"creative"}
+              loop={true}
+              direction="vertical"
+              grabCursor={true}
+              creativeEffect={{
+                prev: {
+                  shadow: true,
+                  translate: [0, -400, 0],
+                },
+                next: {
+                  translate: [0, 0, -400],
+                },
+              }}
+              controller={{ control: secondSwiper }}
+              modules={[Controller, EffectCreative]}
+              className="mySwiper w-72 h-96 text-white border-t-2 border-cyan-600"
             >
-              <h2 className="w-4/5 text-cyan-400 text-xl font-bold mb-8">
-                {eve.title.toUpperCase()}
-              </h2>
-              <p className="w-full h-full text-md text-pretty text-justify">
-                {eve.description}
-              </p>
-              <div className="w-full text-sm text-center flex justify-between">
-                <p className="w-full flex flex-col justify-between">
-                  <span className="font-semibold text-cyan-500">
-                    {eve.event_date}
-                  </span>
-                  <span className="font-bold">Event Date </span>
-                </p>
-                <p className="w-full flex flex-col justify-between border-l border-gray-600">
-                  <span className="font-semibold text-cyan-500">
-                    {eve.participants}
-                  </span>
-                  <span className="font-bold">Participants </span>
-                </p>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      ) : (
-        <Swiper
-          onSwiper={setFirstSwiper}
-          effect={"creative"}
-          loop={true}
-          direction="horizontal"
-          grabCursor={true}
-          creativeEffect={{
-            prev: {
-              shadow: true,
-              translate: [0, 0, -400],
-            },
-            next: {
-              translate: [400, 0, 0],
-            },
-          }}
-          controller={{ control: secondSwiper }}
-          modules={[Controller, EffectCreative]}
-          className="mySwiper w-72 h-96 text-white border-r-2 border-cyan-600"
-        >
-          {Events.map((eve) => (
-            <SwiperSlide
-              key={eve.title}
-              className="flex w-80 text-center p-4 flex-col items-center justify-between bg-slate-900 border-l border-y border-gray-600 rounded-s-lg"
+              {Events.map((eve) => (
+                <SwiperSlide
+                  key={eve.title}
+                  className="flex w-72 text-center p-4 flex-col items-center justify-between bg-slate-900 border-b border-x border-gray-600 rounded-b-lg"
+                >
+                  <h2 className="w-4/5 text-cyan-400 text-xl font-bold mb-8">
+                    {eve.title.toUpperCase()}
+                  </h2>
+                  <p className="w-full h-full text-md text-pretty text-justify">
+                    {eve.description}
+                  </p>
+                  <div className="w-full text-sm text-center flex justify-between">
+                    <p className="w-full flex flex-col justify-between">
+                      <span className="font-semibold text-cyan-500">
+                        {eve.event_date}
+                      </span>
+                      <span className="font-bold">Event Date </span>
+                    </p>
+                    <p className="w-full flex flex-col justify-between border-l border-gray-600">
+                      <span className="font-semibold text-cyan-500">
+                        {eve.participants}
+                      </span>
+                      <span className="font-bold">Participants </span>
+                    </p>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          ) : (
+            <Swiper
+              onSwiper={setFirstSwiper}
+              effect={"creative"}
+              loop={true}
+              direction="horizontal"
+              grabCursor={true}
+              creativeEffect={{
+                prev: {
+                  shadow: true,
+                  translate: [0, 0, -400],
+                },
+                next: {
+                  translate: [400, 0, 0],
+                },
+              }}
+              controller={{ control: secondSwiper }}
+              modules={[Controller, EffectCreative]}
+              className="mySwiper w-72 h-96 text-white border-r-2 border-cyan-600"
             >
-              <h2 className="w-4/5 text-cyan-400 text-xl font-bold mb-8">
-                {eve.title.toUpperCase()}
-              </h2>
-              <p className="w-full h-full text-md text-pretty text-justify">
-                {eve.description}
-              </p>
-              <div className="w-full text-sm text-center flex justify-between">
-                <p className="w-full flex flex-col justify-between">
-                  <span className="font-semibold text-cyan-500">
-                    {eve.event_date}
-                  </span>
-                  <span className="font-bold">Event Date </span>
-                </p>
-                <p className="w-full flex flex-col justify-between border-l border-gray-600">
-                  <span className="font-semibold text-cyan-500">
-                    {eve.participants}
-                  </span>
-                  <span className="font-bold">Participants </span>
-                </p>
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      )}
-    </div>
+              {Events.map((eve) => (
+                <SwiperSlide
+                  key={eve.title}
+                  className="flex w-80 text-center p-4 flex-col items-center justify-between bg-slate-900 border-l border-y border-gray-600 rounded-s-lg"
+                >
+                  <h2 className="w-4/5 text-cyan-400 text-xl font-bold mb-8">
+                    {eve.title.toUpperCase()}
+                  </h2>
+                  <p className="w-full h-full text-md text-pretty text-justify">
+                    {eve.description}
+                  </p>
+                  <div className="w-full text-sm text-center flex justify-between">
+                    <p className="w-full flex flex-col justify-between">
+                      <span className="font-semibold text-cyan-500">
+                        {eve.event_date}
+                      </span>
+                      <span className="font-bold">Event Date </span>
+                    </p>
+                    <p className="w-full flex flex-col justify-between border-l border-gray-600">
+                      <span className="font-semibold text-cyan-500">
+                        {eve.participants}
+                      </span>
+                      <span className="font-bold">Participants </span>
+                    </p>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          )}
+        </div>
+      </div>
+    </>
   );
 };
 
