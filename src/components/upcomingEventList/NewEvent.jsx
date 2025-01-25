76db15618de6e821ab87805e9ac8971a1dd73582
ManmohanSinghRaghav <manmohan0singh0@gmail.com>
2025-01-25 16:58:36 +0530

    fix: correct SVG attribute casing in NewEvent component

--- src/components/upcomingEventList/NewEvent.jsx
+++ src/components/upcomingEventList/NewEvent.jsx
@@ -74,9 +74,9 @@ const NewEvent = () => {
                       xmlns="http://www.w3.org/2000/svg"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinenjoin="round"
+                        strokeWidth="2"
                         d="M5 12h14M12 5l7 7-7 7"
                       />
                     </svg>
